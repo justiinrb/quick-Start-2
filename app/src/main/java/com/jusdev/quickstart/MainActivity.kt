948ae6e7d729921ea9ae76b0f9befe84071f5c4b
Fazet <contact0faztweb.com>
2022-07-22 17:59:15 -0500

    se agrego Parametros

--- app/src/main/java/com/jusdev/quickstart/MainActivity.kt
+++ app/src/main/java/com/jusdev/quickstart/MainActivity.kt
@@ -8,24 +8,39 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-val hola: String = "hola"
-var numero = 1
-var palabra: String="malo"
+    val hola: String = "hola"
+    var numero = 1
+    var palabra: String = "malo"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mostrarhola()
-        mostardatos()
-        }
-fun mostrarhola(){
-    Log.v("mostrar","Hola kotlin")
-}
-    fun mostardatos() {
-Log.v("datos","nombre , apellido")
+        SumaNum(15,6)
+        mostrarDatos("Justin","Rocafuerte",17)
+diviNum(20, 2)
     }
 
+    fun SumaNum(num1:Int,num2:Int){
+        var suma = num1+num2
+
+        Log.v("re","Resultado "+suma)
+
     }
+    fun mostrarDatos(nombre:String,apellido:String,edad:Int){
+
+        var nombre = nombre
+        var  apellido = apellido
+        var edad = edad
+Log.v("datos","mostrar "+nombre +" "+apellido +" " +edad)
+    }
+
+fun diviNum(mn1:Int,mn2:Int){
+    var division = mn1/mn2
+    Log.v("Alt","Resultado "+division)
+
+}
+
+}
 
 
 
