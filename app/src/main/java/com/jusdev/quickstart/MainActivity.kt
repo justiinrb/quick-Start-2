1fb9d375eae0fecfb87587d089d064e35d467fe1
Fazet <contact0faztweb.com>
2022-07-27 17:37:47 -0500

    se agrego un textView

--- app/src/main/java/com/jusdev/quickstart/MainActivity.kt
+++ app/src/main/java/com/jusdev/quickstart/MainActivity.kt
@@ -3,43 +3,20 @@ package com.jusdev.quickstart
 import android.os.Bundle
 import android.util.Log
 import android.util.Log.println
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    val hola: String = "hola"
-    var numero = 1
-    var palabra: String = "malo"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        SumaNum(15,6)
-        mostrarDatos("Justin","Rocafuerte",17)
-diviNum(20, 2)
+
+        val txt =  findViewById<TextView>(R.id.Text_to)
+        txt.text = " justin rocaf"
     }
-
-    fun SumaNum(num1:Int,num2:Int){
-        var suma = num1+num2
-
-        Log.v("re","Resultado "+suma)
-
-    }
-    fun mostrarDatos(nombre:String,apellido:String,edad:Int){
-
-        var nombre = nombre
-        var  apellido = apellido
-        var edad = edad
-Log.v("datos","mostrar "+nombre +" "+apellido +" " +edad)
-    }
-
-fun diviNum(mn1:Int,mn2:Int){
-    var division = mn1/mn2
-    Log.v("Alt","Resultado "+division)
-
-}
-
 }
 
 
