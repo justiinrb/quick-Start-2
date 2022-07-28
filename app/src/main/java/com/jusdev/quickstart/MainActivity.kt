68eb5089f0760502180dcd2654c4c3732679f6a8
Fazet <contact0faztweb.com>
2022-07-27 20:32:01 -0500

    condicion If Else

--- app/src/main/java/com/jusdev/quickstart/MainActivity.kt
+++ app/src/main/java/com/jusdev/quickstart/MainActivity.kt
@@ -13,11 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notas(9.5,9.5,8.0,7.0,4.0)
+    }
 
-        val txt =  findViewById<TextView>(R.id.Text_to)
-        txt.text = " justin rocaf"
+    fun notas (N1:Double,N2:Double,N3:Double,N4:Double,re:Double){
+
+        var par = (N1+N2+N3+N4)/4
+
+
+        if(par<=6){
+            Log.v("ver","No aprobo el parcial tiene "+par)
+        }else{
+
+            Log.v("tiene","aprobo con "+par)
+        }
+
 
     }
+
+
+
 }
 
 
