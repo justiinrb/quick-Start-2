04564714423dd2c14905a0b807b099f904adfc79
Fazet <contact0faztweb.com>
2022-08-01 18:23:46 -0500

    Retronos return

--- app/src/main/java/com/jusdev/quickstart/MainActivity.kt
+++ app/src/main/java/com/jusdev/quickstart/MainActivity.kt
@@ -13,23 +13,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        notas(9.5,9.5,8.0,7.0,4.0)
+        val Texto = findViewById<TextView>(R.id.Text_to) as TextView
+
+        var resultado: Int = sumaNumeros(45 ,2 ,3 )
+        Texto.text = " El valor a retornar es "+resultado
+          
     }
 
-    fun notas (N1:Double,N2:Double,N3:Double,N4:Double,re:Double){
 
-        var par = (N1+N2+N3+N4)/4
+    fun sumaNumeros (Num1:Int, Num2:Int,Num3:Int) :Int{
+    var sum : Int = Num1+Num2+Num3
+    return sum
+}
+    fun Datos_P(Nombre: String,Apellido:String):String{
 
+        val nombre = Nombre
+        val apellido = Apellido
 
-        if(par<=6){
-            Log.v("ver","No aprobo el parcial tiene "+par)
-        }else{
-
-            Log.v("tiene","aprobo con "+par)
-        }
+        return nombre ; apellido
 
 
     }
+
+
+
+
+
+
 
 
 
