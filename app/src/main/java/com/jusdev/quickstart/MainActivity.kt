d7fac8b54fc0a119524aace04faa618bb8f58d87
Fazet <contact0faztweb.com>
2022-08-04 18:28:46 -0500

    arreglos

--- app/src/main/java/com/jusdev/quickstart/MainActivity.kt
+++ app/src/main/java/com/jusdev/quickstart/MainActivity.kt
@@ -9,36 +9,24 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+lateinit var texto :TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val Texto = findViewById<TextView>(R.id.Text_to) as TextView
 
-var resulta : String = notas(7,8,9,9)
-
-        Texto.text = " el resultado es "+resulta
-    }
-
-    fun notas (Num1:Int, Num2:Int,Num3:Int,Num4:Int) :String{
-
-        var res : Int = (Num1+Num2+Num3+Num4)/4
-
-        if(res>=7){
-
-            return "feli"
-
-        }else{
-
-            return "no aprobo"
+        var numero = 5
+        var arreglos = arrayOf(45,66,55)
 
 
+        Texto.text = arreglos[0].toString()
     }
 
 }
 
 
-}
+
 
 
 
