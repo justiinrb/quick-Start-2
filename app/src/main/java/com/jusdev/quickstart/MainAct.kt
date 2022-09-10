f98d6328993cf59463af27c172430fb1ac7a3cb9
Fazet <contact0faztweb.com>
2022-09-10 17:01:00 -0500

    bucle-For

--- app/src/main/java/com/jusdev/quickstart/MainAct.kt
+++ app/src/main/java/com/jusdev/quickstart/MainAct.kt
@@ -10,28 +10,25 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainAct : AppCompatActivity() {
     lateinit var texto: TextView
+    //te permite inicializar una propiedad no anulable por fuera del constructor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val Texto = findViewById<TextView>(R.id.Text_to) as TextView
 
 
-        var numero = 0
-        do{
-            //instruccion
-          Log.v("men","Hola do while")
-            numero++
-        } while (numero <= 4)
+       //ciclos - loops - bucles
+       //cada elemento dentro de una collecion
+       //item in collection
+       var arreglo = arrayOf(100,200,300)
 
-        var num =   2
-        var i = 1
-        do {
-            Log.v("inst","2 * $i = " + num * i  )
-            i++
-        }while (i< 11)
+       for(valor: Int in arreglo){
+           //instruccion
+           Log.v("fo",""+valor)
 
-    }
+   }
+}
+
 
 
 }
