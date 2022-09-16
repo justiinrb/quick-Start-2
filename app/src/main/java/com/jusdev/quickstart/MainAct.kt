7f4de0fec03bd8a5453886de887cbf13a3081520
Fazet <contact0faztweb.com>
2022-09-16 18:48:25 -0500

    Enum-Class

--- app/src/main/java/com/jusdev/quickstart/MainAct.kt
+++ app/src/main/java/com/jusdev/quickstart/MainAct.kt
@@ -17,16 +17,24 @@ class MainAct : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 texto=findViewById<TextView>(R.id.Text_to) as TextView
 
-var Articulo1 = Articulo1(1,"Computadora",200f)
+var Articulo1 = Articulo1(1,"Computadora",Precio.Barato)
 
         Log.v("ar",""+Articulo1)
 Articulo1.desc = "Laptop"
+        Articulo1.Precio = Precio.Caro
         Log.v("lp",""+Articulo1)
 
 
     }
-data class Articulo1 (var codigo : Int,var desc : String, var Precio: Float)
+data class Articulo1 (var codigo : Int,var desc : String, var Precio: Precio)
+//enum constantes
+    enum class Precio{
+        Barato,
+        Justo,
+        Caro
 
+
+    }
 
 
 
